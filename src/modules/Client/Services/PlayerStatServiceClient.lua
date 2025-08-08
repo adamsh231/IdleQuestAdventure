a0be8ef6ecdcbd953bc47d16d587bb174f7072fa
adamsh231 <adamsyarif219@gmail.com>
2025-08-08 21:36:24 +0700

    oyi

--- src/modules/Client/Services/PlayerStatServiceClient.lua
+++ src/modules/Client/Services/PlayerStatServiceClient.lua
@@ -16,8 +16,6 @@ function PlayerStatServiceClient:TriggerListener()
 
 	local getPlayerStatEvent = ReplicatedStorage.RemoteEvents.PlayerStatEvent
 
-	self:LoadGUI(PlayerStatConstant.GetDefaultStats()) -- Only for testing purposes
-
 	PromiseChild(getPlayerStatEvent, "GetPlayerStat")
 		:Then(function(getPlayerStat)
 			getPlayerStat.OnClientEvent:Connect(function(statValue)
@@ -27,30 +25,29 @@ function PlayerStatServiceClient:TriggerListener()
 		:Catch(function(err)
 			warn("Failed to get GetPlayerStat event:", err)
 		end)
+
+	self._triggerButtonXP()
 end
 
--- Only for testing purposes
 local Player = game:GetService("Players").LocalPlayer
-local XPGUI = Player:WaitForChild("PlayerGui"):WaitForChild("XPGUI")
-local ButtonXPGUI = Player:WaitForChild("PlayerGui"):WaitForChild("ButtonXPGUI"):WaitForChild("AddXP")
-local currentXP = 0
-function PlayerStatServiceClient:LoadGUI(stats)
-	assert(self._serviceBag, "Not initialized")
+function PlayerStatServiceClient._triggerButtonXP()
 
-	local XPBar = XPGUI.XP.Bar
-	if not XPBar then
-		print("XP Bar not found in XPGUI")
-	else
-		XPBar.Size = UDim2.new(0, 0, 1, 0)
-	end
-
-	ButtonXPGUI.MouseButton1Click:Connect(function()
-		if currentXP >= 100 then
-			currentXP = 0
-		end
-		XPBar.Size = UDim2.new(currentXP / 100, 0, 1, 0)
-		currentXP += 10
-	end)
+	PromiseChild(Player, "PlayerGui")
+		:Then(function(playerGui)
+			PromiseChild(playerGui, "ScreenGui")
+				:Then(function(screenGui)
+					local addXPButton = screenGui:FindFirstChild("AddXP")
+					addXPButton.MouseButton1Click:Connect(function()
+						print("AddXP button clicked")
+					end)
+				end)
+				:Catch(function(err)
+					warn("Failed to get ScreenGui:", err)
+				end)
+		end)
+		:Catch(function(err)
+			warn("Failed to get PlayerGui:", err)
+		end)
 end
 
 return PlayerStatServiceClient
